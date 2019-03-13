c65231b241c00ed024920e675b8d3cef1f5e02e3
Adam Frey <adam@adamfrey.me>
2019-03-13 18:28:22 -0400

    Get correct function name

--- keyboards/cospad/keymaps/alex-cospad/keymap.c
+++ keyboards/cospad/keymaps/alex-cospad/keymap.c
@@ -86,5 +86,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void keyboard_post_init_user(void) {
   // Underglow
   rgblight_enable();
-  rgblight_set_rgb_purple();
+  rgblight_setrgb_purple();
 }
